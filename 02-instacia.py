159a99069e6a274b87582cbe4fe09fb527f39758
Xanpis <wellington.org.com@gmail.com>
2024-08-14 08:56:27 -0300

    up

---
+++ 02-instacia.py
@@ -0,0 +1,25 @@
+from shutil import move
+
+
+class Movie:
+   name = ""
+   yearLaunch = 0
+   includedPlan = False
+   note = 0
+   durationMinutes = 0
+   # sobre escrevendo o a referencia de uma classe 
+   def __str__(self):
+      return f"filme: {self.name}"
+
+# primeiro filme
+movie = Movie()
+movie.name = "Super Mario"
+movie.yearLaunch = 2023
+movie.includedPlan = False
+movie.note = 5.0
+movie.durationMinutes = 150
+
+print(movie)
+
+print("##Dados do filme##")
+print(f"Nome do filme = {movie.name} \nAno de lançamento = {movie.yearLaunch}")
---
+++ 03-construct.py
@@ -0,0 +1,17 @@
+
+class Movie:
+   def __init__(self, name, yearLaunch, includedPlan = False, note = 0 , durationMinutes = 150):
+      self.name = name
+      self.yearLaunch = yearLaunch
+      self.includedPlan = includedPlan
+      self.note = note
+      self.durationMinutes = durationMinutes
+   # sobre escrevendo o a referencia de uma classe 
+   def __str__(self):
+      return f"filme: {self.name}"
+
+
+movie = Movie('Fifa', 2023)
+movie2 = Movie()
+print("##Dados do filme##")
+print(f"Nome do filme = {movie.name} \nAno de lançamento = {movie.yearLaunch} \nDuração: {movie.durationMinutes}")
