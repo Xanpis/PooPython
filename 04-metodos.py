8b413c8b1dbffa564d2e14671935c6cc05a2d04a
Xanpis <wellington.org.com@gmail.com>
2024-08-14 09:52:35 -0300

    up

---
+++ 04-metodos.py
@@ -0,0 +1,28 @@
+
+class Movie:
+   def __init__(self, name, yearLaunch, includedPlan = False, note = 0 , durationMinutes = 'Padrão'):
+      self.name = name
+      self.yearLaunch = yearLaunch
+      self.includedPlan = includedPlan
+      self.note = note
+      self.durationMinutes = durationMinutes
+   # sobre escrevendo o a referencia de uma classe 
+   def __str__(self):
+      return f"filme: {self.name}"
+
+   def technical_sheet(self):
+      print("## Dados do filme ##")
+      print(f'Nome do filme = {self.name}')
+      print(f'Ano de lançamento = {self.yearLaunch} ')
+      print(f'Incluso no plano = {self.includedPlan}')
+      print(f'Nota = {self.note}')
+      print(f'Duração em minutos = {self.durationMinutes}\n')
+
+
+mario = Movie('Mario Gross', 2020 ,False, 8.5, 200)
+top_gun = Movie('Top Gun Marverik', 2020 ,False, 8.5)
+
+mario.technical_sheet()
+top_gun.technical_sheet()
+
+
