6d30dc58d123aa08d784a7ab0cb7fa70873c8e35
Xanpis <wellington.org.com@gmail.com>
2024-08-17 18:43:34 -0300

    up

--- exercicio/02-Comercio/01-produto.py
+++ exercicio/02-Comercio/01-produto.py
@@ -2,5 +2,24 @@ class Produto:
    def __init__(self,nome,preco):
       self.nome = nome
       self.preco = preco
-
    
+   def desconto(self,porcent ):
+      self.descontoTotal = self.preco - (self.preco * porcent / 100)
+
+   def __str__(self):
+      return f"Nome: {self.nome}"
+   
+   def empimeProdoto(self):
+      print(f'Nome do Produto: {self.nome}')
+      print(f'Preço: {self.preco}')
+      print(f'Desconto: {self.descontoTotal}')
+      
+   
+
+arroz = Produto('Arroz',5)
+arroz.desconto(50)
+arroz.empimeProdoto()
+
+feijao = Produto('Feijao',8.00)
+feijao.desconto(10)
+feijao.empimeProdoto()
