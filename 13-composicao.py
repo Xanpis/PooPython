68f97a9e6576d2cb933528ebe7656bfe7e1de989
Xanpis <wellington.org.com@gmail.com>
2024-08-30 20:18:41 -0300

    up

---
+++ 13-composicao.py
@@ -0,0 +1,42 @@
+class Animal():
+    def __init__(self, name, categoria) -> None:
+        self.name = name
+        self.categoria = categoria
+       
+class Fish(Animal):
+    color = " "
+    
+class Parrot (Animal):
+    color = " "
+    
+    
+class Zoo:
+    def __init__(self) -> None:
+        self.animal_dict = {}
+        
+    def add_animal(self, animal):
+        self.animal_dict[animal.name] = animal.categoria
+        
+        
+    def total_of_category(self,category):
+        result = 0 
+        for animal in self.animal_dict.values():
+            if animal == category:
+                result +=1
+                
+        return f"No nosso zoológico temos: {result} quantidade de: {category} "
+
+
+zoo = Zoo()
+
+peixe = Fish("Nemo","peixe")
+print(vars(peixe))
+
+papagaio = Parrot("Lorro","aves")
+print(vars(papagaio))
+
+zoo.add_animal(peixe)
+zoo.add_animal(papagaio)
+
+print(zoo.total_of_category("aves"))
+print(zoo.total_of_category("peixe"))
